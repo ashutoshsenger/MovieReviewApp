6f0a2761a94ccbc112327ba6c170d30b1324e792
ASHUTOSH SENGER <ashutoshsenger09@gmail.com>
2020-09-13 22:07:58 +0530

    t

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/movie-rating-app'));
+app.use(express.static(__dirname + '/dist/movie-rating-app'));
 
+// Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/movie-rating-app/'}
-  );
+  res.sendFile(path.join(__dirname + '/dist/movie-rating-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
